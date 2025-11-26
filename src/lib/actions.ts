e2100a64ded1655aab110f43263457a922fd4057
Mir Abrar Ali <alimirabrar@gmail.com>
2025-11-26 11:19:46 +0000

    new error ughhh

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import 'dotenv/config';
 import { revalidatePath } from 'next/cache';
 import { assessLoanEligibility } from '@/ai/flows/loan-eligibility-assessment';
 import { addNewUser, findUserByCredentials, getUser, updateUser, getAllUsers } from '@/lib/data';
